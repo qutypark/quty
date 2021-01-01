cb0deeef87d7dac61f44f7613edcef7a3397d5fd
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-01-01 19:36:50 +0900

    Create MapReduce.py

---
+++ hadoop/MapReduce/MapReduce.py
@@ -0,0 +1,16 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+Class RatingsBreakdown(MRJob):
+  def steps(self):
+    return [MRStep(mapper = self.Mapper_get_ratings,
+                   reducer = self.Reduce_count_ratings)
+           ]
+  def Mapper_get_ratings(self,_,line):
+    (userID,MovieID,ratings,timestamp) = line.split('/t')
+    yield ratings,1
+  
+  def Reduce_count_ratings(self,key,values):
+    yield key, sum(values)
+if __name__ == '__main__':
+  RatingsBreakdown.run()
