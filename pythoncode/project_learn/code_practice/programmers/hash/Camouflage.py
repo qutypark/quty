5da065269460975227e16e5b6cc8583b7778287a
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-06-01 11:34:39 +0900

    Update Camouflage.py

--- pythoncode/project_learn/code_practice/programmers/hash/Camouflage.py
+++ pythoncode/project_learn/code_practice/programmers/hash/Camouflage.py
@@ -1,4 +1,6 @@
-#스파이가 가진 의상들이 담긴 2차원 배열 clothes가 주어질 때 서로 다른 옷의 조합의 수를 return
+# 프로그래머스_해쉬_위장
+
+# 스파이가 가진 의상들이 담긴 2차원 배열 clothes가 주어질 때 서로 다른 옷의 조합의 수를 return
 # clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
 
 # 조합: (a+1)(b+1)(c+1)-1
