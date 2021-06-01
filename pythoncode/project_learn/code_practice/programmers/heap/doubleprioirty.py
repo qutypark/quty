4625e6625973b707231d968e0914aceed4fb1910
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-06-01 12:02:35 +0900

    Create doubleprioirty.py

---
+++ pythoncode/project_learn/code_practice/programmers/heap/doubleprioirty.py
@@ -0,0 +1,28 @@
+# 프로그래머스_힙_이중우선순위큐
+
+# I 숫자	: 큐에 주어진 숫자를 삽입
+# D 1	   : 큐에서 최댓값을 삭제 
+# D -1	 : 큐에서 최솟값을 삭제
+
+# 이중 우선순위 큐가 할 연산 operations가 매개변수로 주어질 때, 모든 연산을 처리한 후 큐가 비어있으면 [0,0] 비어있지 않으면 [최댓값, 최솟값]을 return
+
+# operaion = ["I 16","D 1"]
+
+import heapq
+def solution(lis):
+    ans=[]
+    for e in lis:
+        e0 = e.split()[0]
+        e1 = int(e.split()[1])
+        if e0=='I':
+            heapq.heappush(ans,e1)
+        elif e0=='D' and e1==1:
+            try:ans.pop(-1)
+            except: pass
+        elif e0=='D' and e1==-1:
+            try:ans.pop(0)
+            except: pass
+    if len(ans)>1:
+        return [max(ans),min(ans)]
+    else:
+        return [0,0]
