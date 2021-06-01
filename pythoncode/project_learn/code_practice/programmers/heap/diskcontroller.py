8706996ae81a77491dcb139b05e385e7be5d053b
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-06-01 12:00:36 +0900

    Update diskcontroller.py

--- pythoncode/project_learn/code_practice/programmers/heap/diskcontroller.py
+++ pythoncode/project_learn/code_practice/programmers/heap/diskcontroller.py
@@ -1,5 +1,5 @@
 # 프로그래머스_힙_디스크 컨트롤러
-# 핵심: First Come First Served(FCFS)이 아닌 ->  " Shortest Job First(SJF)"
+# 핵심: First Come First Served(FCFS)이 아닌 ->  " Shortest Job First(SJF)" 
 
 # 각 작업에 대해 [작업이 요청되는 시점, 작업의 소요시간]을 담은 2차원 배열 jobs가 매개변수로 주어질 때, 
 # 작업의 요청부터 종료까지 걸린 시간의 평균을 가장 줄이는 방법으로 처리하면 평균이 얼마가 되는지 return.
@@ -13,6 +13,7 @@ def solution(jobs):
     h = []
     while i < len(jobs):
         for e in jobs:
+            # 소요시간이 짧은 대로 리스트 추가 ( " Shortest Job First(SJF)" )
             if start < e[0] <= now: 
                 heapq.heappush(h, [e[1],e[0]]) 
         if len(h)>0:
