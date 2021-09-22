ad3cb3dbeaf6e0a4760f0f24f87a017a3fd573be
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-09-22 09:21:07 +0900

    Update Vowel_Weather Observation Station 6.sql

--- sql/hackerrank/Vowel_Weather Observation Station 6.sql
+++ sql/hackerrank/Vowel_Weather Observation Station 6.sql
@@ -3,3 +3,9 @@ Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from 
 */
 select distinct(city) from station
 where lower(substr(city,1,1)) in ('a','e','i','o','u');
+
+/*
+regexp
+*/
+select distinct(city) from station
+where city regexp '^[aeiouAEIOU]';
