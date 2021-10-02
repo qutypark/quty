3976e87a2e8adca037e1a1319ec31a7c02c50295
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-02 15:26:44 +0900

    Create unionall_Weather Observation Station 5.sql

---
+++ sql/hackerrank/unionall_Weather Observation Station 5.sql
@@ -0,0 +1,10 @@
+/*
+Query the two cities in STATION with the shortest and longest CITY names, 
+as well as their respective lengths (i.e.: number of characters in the name). 
+If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+*/
+
+select * from (select city, length(city) as citylen from station order by citylen asc, city asc limit 1) as a
+union
+select * from (select city, length(city) as citylen from station order by citylen desc, city desc limit 1) as b;
+
