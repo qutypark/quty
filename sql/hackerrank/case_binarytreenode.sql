4839101b77be3bc6bbff644d970d9c6b40b37b99
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-02 15:50:52 +0900

    Create case_binarytreenode.sql

---
+++ sql/hackerrank/case_binarytreenode.sql
@@ -0,0 +1,15 @@
+/*
+Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.
+*/
+
+select distinct(N) as node, (case when P is NULL then "Root"
+        when N not in (select distinct(P) from BST where p is not NULL) then "Leaf"
+        else "Inner"
+        end)
+from BST
+order by node asc;
+
