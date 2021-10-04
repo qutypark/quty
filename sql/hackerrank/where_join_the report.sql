bf6c0bb5697ec3372e6802e5c0de998d4bef7835
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-04 13:49:38 +0900

    Update where_join_the report.sql

--- sql/hackerrank/where_join_the report.sql
+++ sql/hackerrank/where_join_the report.sql
@@ -6,3 +6,7 @@ where students.marks between grades.min_mark and grades.max_mark
 order by grades.grade desc, students.marks desc, name;
 
 
+select (case when g.grade<8 then null else s.name end) as name, g.grade, s.marks
+from students as s
+join grades as g on s.marks between g.min_mark and g.max_mark
+order by g.grade desc, name;
