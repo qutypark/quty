8fdbd06014dab01d61bebf69a7732db86d7a1d6b
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-19 21:15:35 +0900

    Create all.sql

---
+++ sql/postgresql/all.sql
@@ -0,0 +1,19 @@
+-- returns the average lengths of all films grouped by film rating:
+
+SELECT
+    film_id,
+    title,
+    length
+FROM
+    film
+WHERE
+    length > ALL (
+            SELECT
+                ROUND(AVG (length),2)
+            FROM
+                film
+            GROUP BY
+                rating
+    )
+ORDER BY
+    length;
