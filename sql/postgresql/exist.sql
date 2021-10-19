d39a9a4a2abeb67226a3edf046c79adc9242188c
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-19 21:24:36 +0900

    Update exist.sql

--- sql/postgresql/exist.sql
+++ sql/postgresql/exist.sql
@@ -1,9 +1,24 @@
+-- The EXISTS operator is a boolean operator that tests for existence of rows in a subquery.
+
 -- Find customers who have at least one payment whose amount is greater than 11.
 
 SELECT first_name,
        last_name
 FROM customer c
 WHERE EXISTS
+    (SELECT 1
+     FROM payment p
+     WHERE p.customer_id = c.customer_id
+       AND amount > 11 )
+ORDER BY first_name,
+         last_name;
+         
+-- The following example returns customers have not made any payment that greater than 11.         
+
+SELECT first_name,
+       last_name
+FROM customer c
+WHERE NOT EXISTS
     (SELECT 1
      FROM payment p
      WHERE p.customer_id = c.customer_id
