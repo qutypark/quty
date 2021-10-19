3808c6bcfbe120bc9119494385b4b262bc4888e1
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-19 17:03:38 +0900

    Create grouping_sets.sql

---
+++ sql/posgresql/grouping_sets.sql
@@ -0,0 +1,36 @@
+-- brand.segments / brand / segment 
+
+SELECT
+    brand,
+    segment,
+    SUM (quantity)
+FROM
+    sales
+GROUP BY
+    GROUPING SETS (
+        (brand, segment),
+        (brand),
+        (segment),
+        ()
+    );
+    
+    
+-- brand.segments / brand / segment 
+
+SELECT
+	GROUPING(brand) grouping_brand,
+	GROUPING(segment) grouping_segment,
+	brand,
+	segment,
+	SUM (quantity)
+FROM
+	sales
+GROUP BY
+	GROUPING SETS (
+		(brand),
+		(segment),
+		()
+	)
+ORDER BY
+	brand,
+	segment;
