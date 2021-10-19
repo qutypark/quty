c5d19274029650b7e2e58f13545b322e8798170b
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-19 21:32:25 +0900

    Update and rename update_join.sql to update+delete_join.sql

--- sql/postgresql/update+delete_join.sql
+++ sql/postgresql/update+delete_join.sql
@@ -4,3 +4,10 @@ UPDATE product
 SET net_price = price - price * discount
 FROM product_segment
 WHERE product.segment_id = product_segment.id;
+
+
+-- delete the contacts in the contacts table with the phone number exists in the blacklist table:
+
+DELETE FROM contacts 
+USING blacklist
+WHERE contacts.phone = blacklist.phone;
