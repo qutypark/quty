fb4d852201c7aa1e174846ddbb4d5be9d8f6808f
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-19 20:51:18 +0900

    Create rollup+extract.sql

---
+++ sql/postgresql/rollup+extract.sql
@@ -0,0 +1,15 @@
+-- finds the number of rental per day, month, and year 
+
+SELECT
+    EXTRACT (YEAR FROM rental_date) y,
+    EXTRACT (MONTH FROM rental_date) M,
+    EXTRACT (DAY FROM rental_date) d,
+    COUNT (rental_id)
+FROM
+    rental
+GROUP BY
+    ROLLUP (
+        EXTRACT (YEAR FROM rental_date),
+        EXTRACT (MONTH FROM rental_date),
+        EXTRACT (DAY FROM rental_date)
+    );
