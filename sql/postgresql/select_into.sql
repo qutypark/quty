82cf14e1f33c12a22caff3611e59a6a0403ba9e2
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-20 20:37:25 +0900

    Create select_into.sql

---
+++ sql/postgresql/select_into.sql
@@ -0,0 +1,15 @@
+-- creates a new table called film_r that contains films with the rating R and rental duration 5 days from the film table.
+
+SELECT
+    film_id,
+    title,
+    rental_rate
+INTO TABLE film_r
+FROM
+    film
+WHERE
+    rating = 'R'
+AND rental_duration = 5
+ORDER BY
+    title;
+
