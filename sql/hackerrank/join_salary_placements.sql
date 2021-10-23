e81cf0c8aa116afbe1715740f925689600c820e8
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-23 17:50:33 +0900

    Update join_salary_placements.sql

--- sql/hackerrank/join_salary_placements.sql
+++ sql/hackerrank/join_salary_placements.sql
@@ -6,4 +6,13 @@ join packages p2 on f.friend_id=p2.id
 where p2.salary>p1.salary
 order by p2.salary;
 
+-- or
+
+
+select s.name as name
+from students as s
+join friends as f on f.id = s.id
+join packages as p on p.id = s.id
+where p.salary < (select p2.salary from packages as p2 where p2.id = f.friend_id)
+order by (select p2.salary from packages as p2 where p2.id = f.friend_id);
 
