5fde22a5cf77b282a56f4e3da8de7f5e40e49552
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-24 12:14:21 +0900

    Update instr_regex_중성화여부.sql

--- sql/programmers/instr_regex_중성화여부.sql
+++ sql/programmers/instr_regex_중성화여부.sql
@@ -14,3 +14,7 @@ SELECT animal_id, name, if(sex_upon_intake regexp 'intact', "X", "O") as net
 from animal_ins
 order by animal_id;
 
+-- case
+select ANIMAL_ID, NAME, (case when SEX_UPON_INTAKE like "%intact%" then "X" else "O" end ) as net
+from ANIMAL_INS
+order by ANIMAL_ID;
