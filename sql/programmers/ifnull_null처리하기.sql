51d5eb8b28176a3401441562139ffe6dc63d9d97
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-24 12:06:22 +0900

    Update ifnull_null처리하기.sql

--- sql/programmers/ifnull_null처리하기.sql
+++ sql/programmers/ifnull_null처리하기.sql
@@ -2,3 +2,8 @@
 SELECT animal_type, IFNULL(name, 'No name'), sex_upon_intake
 from animal_ins
 order by animal_id;
+
+
+select ANIMAL_TYPE, (case when name is null then "No name" else name end) as name, SEX_UPON_INTAKE
+from ANIMAL_INS
+order by animal_id;
