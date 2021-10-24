9e36ff76a1c51d924c4b9455b9c500912c41ac64
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-24 11:02:03 +0900

    Update insert+update.sql

--- sql/postgresql/insert+update.sql
+++ sql/postgresql/insert+update.sql
@@ -8,8 +8,13 @@ DO NOTHING;
 
 -- when inserting a customer that already exists, in this case, you use the UPDATE clause as the action of the INSERT statement as follows:
 
+-- newly inserted value: excluded / existing value: customer(table name)
+-- for example, "Microsoft" has "contact@microsoft.com" , and it want to add "hotline@microsoft.com"
+-- email will be "hotline@microsoft.com;contact@microsoft.com"
+
 INSERT INTO customers (name, email)
 VALUES('Microsoft','hotline@microsoft.com') 
 ON CONFLICT (name) 
 DO 
    UPDATE SET email = EXCLUDED.email || ';' || customers.email;
+   
