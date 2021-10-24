c07b0eec3610b229f0fadfd064527d4916c9b420
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-24 12:08:01 +0900

    Update ifnull_null처리하기.sql

--- sql/programmers/ifnull_null처리하기.sql
+++ sql/programmers/ifnull_null처리하기.sql
@@ -1,9 +1,14 @@
-
+-- ifnull
 SELECT animal_type, IFNULL(name, 'No name'), sex_upon_intake
 from animal_ins
 order by animal_id;
 
-
+-- case
 select ANIMAL_TYPE, (case when name is null then "No name" else name end) as name, SEX_UPON_INTAKE
+from ANIMAL_INS
+order by animal_id;
+
+-- coalesce
+select ANIMAL_TYPE, (coalesce(name, "No name")) as name, SEX_UPON_INTAKE
 from ANIMAL_INS
 order by animal_id;
