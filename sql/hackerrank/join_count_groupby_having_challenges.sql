a054f086f154631eacc0f398c5bc78ff56f8591b
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-26 16:14:59 +0900

    Update join_count_groupby_having_challenges.sql

--- sql/hackerrank/join_count_groupby_having_challenges.sql
+++ sql/hackerrank/join_count_groupby_having_challenges.sql
@@ -11,3 +11,33 @@ cnt in (select counts from (select count(challenge_id) as counts from challenges
 order by cnt desc,c.hacker_id;
 
 
+select ch.hacker_id, hk.name, ch.cnt as challenges_created
+from 
+(select count(challenge_id) as cnt, hacker_id 
+ from challenges 
+ group by hacker_id) as ch
+ left join Hackers as hk 
+ on ch.hacker_id = hk.hacker_id
+ where ch.cnt = 
+ (select count(challenge_id) as ct from challenges group by hacker_id order by ct desc limit 1)
+ or ch.cnt in 
+ (select ct from (select count(challenge_id) as ct from challenges group by hacker_id) as tmp1
+  group by ct having count(*)=1)
+ order by challenges_created desc, ch.hacker_id;
+
+-- error
+
+select ch.hacker_id, hk.name, ch.cnt as challenges_created
+from 
+(select count(challenge_id) as cnt, hacker_id 
+ from challenges 
+ group by hacker_id) as ch
+ left join Hackers as hk 
+ on ch.hacker_id = hk.hacker_id
+ where ch.cnt = 
+ (select count(challenge_id) as ct from challenges group by hacker_id order by ct desc limit 1)
+ or ch.cnt in 
+ /*
+ (select count(challenge_id) as ct from challenges group by hacker_id having count(*)>1)
+ order by challenges_created desc, ch.hacker_id;
+  */
