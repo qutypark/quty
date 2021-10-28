0496890ba2a88ace8541010bc8081eae1dab0b02
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-28 16:27:18 +0900

    Create null_join.sql

---
+++ sql/sqlzoo/null_join.sql
@@ -0,0 +1,9 @@
+-- table teacher has null value on id
+
+-- 1. Select out of following the code which uses a JOIN to show a list of all the departments and number of employed teachers
+
+select dept.name, count(teacher.name) 
+from teacher
+right join dept -- table teacher has null value on id
+on dept.id = teacher.dept
+group by dept.name
