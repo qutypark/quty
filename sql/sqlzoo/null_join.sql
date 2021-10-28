3d8f2d3515655006407488ff7567a22f37154c10
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-28 16:36:07 +0900

    Update null_join.sql

--- sql/sqlzoo/null_join.sql
+++ sql/sqlzoo/null_join.sql
@@ -1,3 +1,4 @@
+-- https://sqlzoo.net/wiki/Using_Null_Quiz
 -- table teacher has null value on id
 
 -- 1. Select out of following the code which uses a JOIN to show a list of all the departments and number of employed teachers
@@ -7,3 +8,5 @@ from teacher
 right join dept -- table teacher has null value on id
 on dept.id = teacher.dept
 group by dept.name
+
+
