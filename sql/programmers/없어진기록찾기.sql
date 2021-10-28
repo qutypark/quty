62ab3b57920a04b1c35420687f45e5e0766613b7
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-10-28 16:54:44 +0900

    Update 없어진기록찾기.sql

--- sql/programmers/없어진기록찾기.sql
+++ sql/programmers/없어진기록찾기.sql
@@ -1,3 +1,11 @@
+select outs.animal_id, outs.name
+from animal_ins as ins
+right join animal_outs as outs 
+on ins.animal_id = outs.animal_id
+where ins.animal_id is null
+order by outs.animal_id;
+
+
 select animal_id, name
 from animal_outs
 where animal_id not in(SELECT o.animal_id
