2b1835adb7a9727dbaa5ca5de04fa6250558a60f
HyEllen <39148080+tododata101@users.noreply.github.com>
2021-12-25 12:25:16 +0900

    Update median_Weather Observation Station 20.sql

--- sql/hackerrank/median_Weather Observation Station 20.sql
+++ sql/hackerrank/median_Weather Observation Station 20.sql
@@ -18,9 +18,12 @@ s.rowindex IN (FLOOR(@rowindex / 2), CEIL(@rowindex / 2));
 or
 */
 
-SELECT round(lat_n,4) from (SELECT @rowid:=@rowid+1 as rowid,lat_n
-FROM station, (SELECT @rowid:=0) as init
-ORDER BY lat_n) as s 
+SELECT 
+ round(lat_n,4) 
+from 
+ (SELECT @rowid:=@rowid+1 as rowid,lat_n
+  FROM station, (SELECT @rowid:=0) as init 
+  ORDER BY lat_n) as s 
 where rowid=(select round(count(*)/2) from station);
 
 
