a32c66cc08f6c1eb08530ae1935f470ab677da62
HyEllen <39148080+tododata101@users.noreply.github.com>
2022-10-09 11:51:45 +0900

    Update case_binarytreenode.sql

--- sql/hackerrank/case_binarytreenode.sql
+++ sql/hackerrank/case_binarytreenode.sql
@@ -5,7 +5,16 @@ Root: If node is root node.
 Leaf: If node is leaf node. *where p is not null*
 Inner: If node is neither root nor leaf node.
 */
+-- 2022-10-09
+select
+    N
+    , case when P is null then 'Root'
+           when N in (select P from bst where p is not null) then 'Inner'
+           else 'Leaf' end as ans
+from bst
+order by 1
 
+-- past
 select distinct(N) as node, (case when P is NULL then "Root"
         when N not in (select distinct(P) from BST where p is not NULL) then "Leaf"
         else "Inner"
